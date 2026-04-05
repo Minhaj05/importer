ce9ac863d4013c1f651bedccaae41b509fed2107
Shafuath Khan <rudrokhan816@gmail.com>
2026-04-06 00:44:55 +0600

    loginscreen.fxmlupdate, carsview.fxml update, carsviewCONTROLLER setup

---
+++ src/main/java/com/spring26/section2/group15/importer/Shafuath/ShafuathCONTROLLER_.java
@@ -0,0 +1,7 @@
+package com.spring26.section2.group15.importer.Shafuath;
+
+public class ShafuathCONTROLLER_
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/spring26/section2/group15/importer/Shafuath/carsviewCONTROLLER.java
@@ -0,0 +1,7 @@
+package com.spring26.section2.group15.importer.Shafuath;
+
+public class carsviewCONTROLLER
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
