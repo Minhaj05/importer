4f6826b19c32e4ad029aaabfcdecb3e503ceaf68
Minhaj Hossain Sadaf <sadafhossain2309@gmail.com>
2026-04-06 23:42:21 +0600

    initial commit

---
+++ src/main/java/com/spring26/section2/group15/importer/Minhaj/customer_loginCONTROLLER.java
@@ -0,0 +1,21 @@
+package com.spring26.section2.group15.importer.Minhaj;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class customer_loginCONTROLLER
+{
+    @javafx.fxml.FXML
+    private PasswordField passwordTextField;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void LoginOnClick(ActionEvent actionEvent) {
+    }
+}
