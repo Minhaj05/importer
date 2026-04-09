e35731581cac44edb3eaf1f8b89b8f1d2a19ee4a
Minhaj Hossain Sadaf <sadafhossain2309@gmail.com>
2026-04-09 23:56:13 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/spring26/section2/group15/importer/Minhaj/service_statusController.java
@@ -0,0 +1,26 @@
+package com.spring26.section2.group15.importer.Minhaj;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class service_statusController
+{
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+    @javafx.fxml.FXML
+    private TableColumn carCol;
+    @javafx.fxml.FXML
+    private TableView serviceTable;
+    @javafx.fxml.FXML
+    private TableColumn currentstatusCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateOnClick(ActionEvent actionEvent) {
+    }
+}
