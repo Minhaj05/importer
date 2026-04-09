4de4f310448cc06c89710c36dec0a7c71ac11279
Minhaj Hossain Sadaf <sadafhossain2309@gmail.com>
2026-04-09 23:17:26 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/spring26/section2/group15/importer/Minhaj/CustomerDashboard.java
@@ -0,0 +1,18 @@
+package com.spring26.section2.group15.importer.Minhaj;
+
+import javafx.event.ActionEvent;
+
+public class CustomerDashboard
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void yesOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void NoOnClick(ActionEvent actionEvent) {
+    }
+}
