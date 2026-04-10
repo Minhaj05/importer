21ecdb1d99aa46a142effbb640b83d0401a98f21
Minhaj Hossain Sadaf <sadafhossain2309@gmail.com>
2026-04-10 23:35:31 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/spring26/section2/group15/importer/Minhaj/service_logoutCONTROLLER.java
@@ -0,0 +1,18 @@
+package com.spring26.section2.group15.importer.Minhaj;
+
+import javafx.event.ActionEvent;
+
+public class service_logoutCONTROLLER
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void NoClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void YesClick(ActionEvent actionEvent) {
+    }
+}
